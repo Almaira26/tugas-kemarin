fed29db6a1d6a9a44f8541e0e6bc2525096f0f21
Almaira26 <almanie86@gmail.com>
2026-05-25 14:03:31 +0800

    file tugas web absensi

---
+++ proses_edit.php
@@ -0,0 +1,19 @@
+<?php
+include'koneksi.php';
+
+if (isset($_POST['update'])) {
+	$id = $_POST['id'];
+	$nama = $_POST ['nama'];
+	$jurusan = $_POST ['jurusan'];
+
+
+	$query = "UPDATE tabel_siswa SET nama = '$nama', jurusan = '$jurusan' WHERE id = '$id'";
+	$hasil = mysqli_query($koneksi, $query);
+
+	if ($hasil) {
+		header("location: tampil.php?pesan=update_berhasil");
+	} else {
+		echo "Gagal mengupdate data: " . mysqli_error ($koneksi);
+	}
+}
+?>
